4768576942086b73c17dcc2c254a45bb960d665a
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-29 18:42:31 -0300

    Construção e Impressão de objetos

---
+++ introducao-a-programacao-web/2-lidando-com-objetos-e-vetores/construcao-e-impressao-de-objetos.js
@@ -0,0 +1,11 @@
+const empresa = {
+    nome: 'Rocketseat',
+    cor: 'roxo',
+    foco: 'programação',
+    endereco: {
+      rua: "Guilherme Gembala",
+      numero: 260,
+    }
+};
+
+console.log(`A empresa ${empresa.nome} está localizada na Rua ${empresa.endereco.rua}, n°${empresa.endereco.numero}.`);
